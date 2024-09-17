893f98ea54403c09440137314110c01c010af550
M-SaenzM <MSAENZMONTANO01@mynbcc.ca>
2024-09-17 19:42:01 -0300

    Search bar component added

--- recipes-finder/src/components/searchBar.jsx
+++ recipes-finder/src/components/searchBar.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
+import '../styles/searchBar.css'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
 const SearchBar = () => {
+    
     return (
         <>
-            <div>
-                Searchbar
+            <div className="search-input">
+                <input placeholder="Search a Recipe"></input>
+                <a><FontAwesomeIcon icon={faSearch} style={{ color: 'grey', fontSize: '24px' }}/></a>
+                
             </div>
         </>
     );
