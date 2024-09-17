9804d2924d9aea21dff78cd38dc03ee07c32dffe
M-SaenzM <MSAENZMONTANO01@mynbcc.ca>
2024-09-17 18:12:52 -0300

    React App Created

---
+++ recipes-finder/src/App.jsx
@@ -0,0 +1,14 @@
+import './styles/App.css'
+import './components/searchBar'
+import SearchBar from './components/searchBar'
+
+function App() {
+
+  return (
+    <>
+      <SearchBar/>
+    </>
+  )
+}
+
+export default App
